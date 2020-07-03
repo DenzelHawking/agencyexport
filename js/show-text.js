2ac39669047a0ffaa2d690b232fba17d07f4c477
DenzelHawking <denzelhawking@gmail.com>
2020-07-03 15:09:33 +0500

    header modified

--- js/show-text.js
+++ js/show-text.js
@@ -1,32 +1,59 @@
-let newsText = document.querySelectorAll('.news-text');
-let newsItem = document.querySelectorAll('.news-item');
-let visibleHeight = newsText[0].clientHeight;
-let showAllText = document.querySelectorAll('.show-hide-description');
+// let newsText = document.querySelectorAll('.news-text');
+// let newsItem = document.querySelectorAll('.news-item');
+// let visibleHeight = newsText[0].clientHeight;
+// let showAllText = document.querySelectorAll('.show-hide-description');
 
-for (let i = 0; i < newsText.length; i++) {
-    let visibleHeight = newsText[i].clientHeight;
-    let fullHeight = newsText[i].scrollHeight;
-    if (fullHeight > visibleHeight) {
-        newsText[i].classList.add('hide-text');
-        newsItem[i].classList.add('shorting-text')
-    }
-};
+// for (let i = 0; i < newsText.length; i++) {
+//     let visibleHeight = newsText[i].clientHeight;
+//     let fullHeight = newsText[i].scrollHeight;
+//     if (fullHeight > visibleHeight) {
+//         newsText[i].classList.add('hide-text');
+//         newsItem[i].classList.add('shorting-text')
+//     }
+// };
 
-showAllText.forEach((elem, index) => {
-    elem.onclick = () => {
-        // console.log(visibleHeight);
-        // console.log(newsText[index].scrollHeight);
+// showAllText.forEach((elem, index) => {
+//     elem.onclick = () => {
+//         // console.log(visibleHeight);
+//         // console.log(newsText[index].scrollHeight);
         
+//         if (elem.classList.contains('openned')) {
+//             newsText[index].style.height = `${visibleHeight}px`;
+//             newsText[index].classList.add('hide-text');
+//             elem.classList.remove('openned');
+//             elem.innerHTML = 'Показать все';
+//         } else {
+//             newsText[index].style.height = `${newsText[index].scrollHeight}px`;
+//             newsText[index].classList.remove('hide-text');
+//             elem.classList.add('openned');
+//             elem.innerHTML = 'Скрыть';
+//         };
+//     };
+// });
+
+
+
+
+let hasSubList = document.querySelectorAll('.has-sub-list');
+let subList = document.querySelectorAll('.sub-list');
+
+
+let arrow = document.createElement('div');
+arrow.classList.add('hide-arrow');
+arrow.innerHTML = '❯';
+
+hasSubList.forEach((elem, index) => {
+    elem.appendChild(arrow);
+    elem.onclick = () => {
         if (elem.classList.contains('openned')) {
-            newsText[index].style.height = `${visibleHeight}px`;
-            newsText[index].classList.add('hide-text');
+            subList[index].style.height = `${0}px`;
+            subList[index].classList.add('hide-text');
             elem.classList.remove('openned');
-            elem.innerHTML = 'Показать все';
         } else {
-            newsText[index].style.height = `${newsText[index].scrollHeight}px`;
-            newsText[index].classList.remove('hide-text');
+            subList[index].style.height = `${subList[index].scrollHeight}px`;
+            subList[index].classList.remove('hide-text');
             elem.classList.add('openned');
-            elem.innerHTML = 'Скрыть';
         };
     };
 });
+
